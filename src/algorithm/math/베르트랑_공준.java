2df0855d5aa502ae0e4329e017b48da1e8b9f27d
chocolaggibbiddori <didrkd684@naver.com>
2023-02-16 17:39:51 +0900

    백준 4948
    더 적은 시간

--- src/algorithm/math/베르트랑_공준.java
+++ src/algorithm/math/베르트랑_공준.java
@@ -6,31 +6,26 @@ public class 베르트랑_공준 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N;
+        boolean[] primeNumberArr = new boolean[246913];
+        primeNumberArr[1] = true;
 
+        for (int i = 2; i < primeNumberArr.length; i++) {
+            if (!primeNumberArr[i]) {
+                for (int j = i * 2; j < primeNumberArr.length; j += i) {
+                    primeNumberArr[j] = true;
+                }
+            }
+        }
+
+        int N;
         while ((N = sc.nextInt()) != 0) {
-            int count = countPrimeNumber(N + 1, N * 2);
+            int count = 0;
+            for (int i = N + 1; i <= N * 2; i++) {
+                if (!primeNumberArr[i]) count++;
+            }
+
             System.out.println(count);
         }
-    }
-
-    private static int countPrimeNumber(int from, int to) {
-        int count = 0;
-        for (int i = from; i <= to; i++) {
-            if (isPrimeNumber(i)) count++;
-        }
-
-        return count;
-    }
-
-    private static boolean isPrimeNumber(int num) {
-        if (num == 1) return false;
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-
-        return true;
     }
 }
 
