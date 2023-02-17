0a267cd88804eb730a3f3fa24563561f21ac1d12
chocolaggibbiddori <didrkd684@naver.com>
2023-02-17 12:49:42 +0900

    백준 10872

---
+++ src/algorithm/recursive/팩토리얼.java
@@ -0,0 +1,24 @@
+package algorithm.recursive;
+
+import java.util.Scanner;
+
+public class 팩토리얼 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(factorial(sc.nextInt()));
+    }
+
+    private static long factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+}
+
+/* input 1              output 1
+   10                   3628800
+ */
+
+/* input 2              output 2
+   0                    1
+ */
