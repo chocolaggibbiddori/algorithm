6139d854758e2fff2d11d31c77ed9ff05cdfe25a
chocolaggibbiddori <didrkd684@naver.com>
2023-02-17 15:16:13 +0900

    백준 11654
    refactor

--- src/algorithm/string/아스키_코드.java
+++ src/algorithm/string/아스키_코드.java
@@ -6,8 +6,7 @@ public class 아스키_코드 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        System.out.println((int) s.charAt(0));
+        System.out.println((int) sc.next().charAt(0));
     }
 }
 
