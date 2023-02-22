deab973cca727906d9653f12c3e940483586fb9d
chocolaggibbiddori <didrkd684@naver.com>
2023-02-22 18:47:50 +0900

    백준 5086

---
+++ src/algorithm/math/배수와_약수.java
@@ -0,0 +1,34 @@
+package algorithm.math;
+
+import java.util.Scanner;
+
+public class 배수와_약수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if (A == 0) break;
+
+            if (A % B == 0) System.out.println("multiple");
+            else if (B % A == 0) System.out.println("factor");
+            else System.out.println("neither");
+        }
+    }
+}
+
+/* input
+8 16
+32 4
+17 5
+0 0
+ */
+
+/* output
+factor
+multiple
+neither
+ */
