9e09013979e73648fef3f1b1152fc6245ce67005
chocolaggibbiddori <didrkd684@naver.com>
2023-02-23 17:56:33 +0900

    백준 1676
    refactor

--- src/algorithm/math/팩토리얼_0의_개수.java
+++ src/algorithm/math/팩토리얼_0의_개수.java
@@ -6,24 +6,28 @@ import java.util.Scanner;
 
 public class 팩토리얼_0의_개수 {
 
+    static Map<Integer, Integer> map;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        map = new HashMap<>();
 
-        Map<Integer, Integer> map = new HashMap<>();
         while (N > 0) {
             int M = N--;
-            while (M % 2 == 0) {
-                map.put(2, map.getOrDefault(2, 0) + 1);
-                M /= 2;
-            }
-            while (M % 5 == 0) {
-                map.put(5, map.getOrDefault(5, 0) + 1);
-                M /= 5;
-            }
+            M = countNumberOfX(M, 2);
+            countNumberOfX(M, 5);
         }
 
         System.out.println(Math.min(map.getOrDefault(2, 0), map.getOrDefault(5, 0)));
+    }
+
+    private static int countNumberOfX(int M, int x) {
+        while (M % x == 0) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+            M /= x;
+        }
+        return M;
     }
 }
 
