86b3e11c26db487d3c7fb9bfaef3f6190327abf6
chocolaggibbiddori <didrkd684@naver.com>
2023-02-24 21:20:40 +0900

    백준 1904

---
+++ src/algorithm/dynamicprogramming/_01타일.java
@@ -0,0 +1,21 @@
+package algorithm.dynamicprogramming;
+
+import java.util.Scanner;
+
+public class _01타일 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+
+        System.out.println(dp[n]);
+    }
+}
+
+/* input    output
+   4        5
+ */
