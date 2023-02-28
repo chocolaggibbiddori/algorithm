9dd2ae0cb99d449c96f2786d7ee1a108a79721d0
chocolaggibbiddori <didrkd684@naver.com>
2023-02-28 21:36:17 +0900

    백준 11758

---
+++ src/algorithm/math/CCW.java
@@ -0,0 +1,44 @@
+package algorithm.math;
+
+import java.util.Scanner;
+
+public class CCW {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] A = new int[4][2];
+        for (int i = 0; i < 3; i++) {
+            A[i][0] = sc.nextInt();
+            A[i][1] = sc.nextInt();
+        }
+        A[3][0] = A[0][0];
+        A[3][1] = A[0][1];
+
+        int ccw = 0;
+        for (int i = 0; i < 3; i++) {
+            ccw += A[i][0] * A[i + 1][1];
+            ccw -= A[i][1] * A[i + 1][0];
+        }
+
+        System.out.println(ccw > 0 ? 1 : ccw == 0 ? 0 : -1);
+    }
+}
+
+/* input 1      output 1
+1 1
+5 5
+7 3             -1
+ */
+
+/* input 2      output 2
+1 1
+3 3
+5 5             0
+ */
+
+/* input 3      output 3
+1 1
+7 3
+5 5             1
+ */
