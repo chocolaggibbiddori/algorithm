6a003af22e540bfc302392a33922ac5bf4c85a5a
chocolaggibbiddori <didrkd684@naver.com>
2023-03-08 12:47:12 +0900

    백준 11866

---
+++ src/algorithm/datastructure/queue/요세푸스_문제_0.java
@@ -0,0 +1,42 @@
+package algorithm.datastructure.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 요세푸스_문제_0 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 1; i <= n; i++) que.add(i);
+
+        int count = 1;
+        while (!que.isEmpty()) {
+            if (count == k) {
+                sb.append(que.poll()).append(", ");
+                count = 1;
+            } else {
+                que.add(que.poll());
+                count++;
+            }
+        }
+
+        sb.replace(sb.length() - 2, sb.length() - 1, ">");
+        System.out.println(sb);
+    }
+}
+
+/* input
+7 3
+ */
+
+/* output
+<3, 6, 2, 7, 5, 1, 4>
+ */
